f1e7de577b7f55b2893ed522c64cf0c1591e278a
mck0324 <dch3030@naver.com>
2023-08-10 23:40:01 +0900

    Controller CRUD맵핑,DTO날짜 Validation

--- src/main/kotlin/com/example/todo/controller/api/todo/TodoApiController.kt
+++ src/main/kotlin/com/example/todo/controller/api/todo/TodoApiController.kt
@@ -1,7 +1,12 @@
 package com.example.todo.controller.api.todo
 
+import com.example.todo.model.http.TodoDto
+import jakarta.validation.Valid
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -18,11 +23,18 @@ class TodoApiController {
     }
     //C
     @PostMapping(path = [""])
-    fun create(@RequestBody) {
+    fun create(@Valid @RequestBody todoDto: TodoDto) {
 
     }
 
     //U
+    @PutMapping(path = [""])
+    fun update(@Valid @RequestBody todoDto: TodoDto) {
 
+    }
     //D
+    @DeleteMapping(path = ["/{index}"])
+    fun delete(@PathVariable(name = "index") _index: Int) {
+
+    }
 }
