6d1329debddf70062ee8837ac7bfbf786c968122
mck0324 <dch3030@naver.com>
2023-08-14 16:59:52 +0900

    Todo Entity생성

--- src/main/kotlin/com/example/todo/service/TodoService.kt
+++ src/main/kotlin/com/example/todo/service/TodoService.kt
@@ -1,12 +1,19 @@
 package com.example.todo.service
 
+import com.example.todo.model.http.TodoDto
+import com.example.todo.respository.TodoRepositoryImpl
 import org.springframework.stereotype.Service
 
 
 @Service
-class TodoService {
+class TodoService (
+        val todoRepositoryImpl: TodoRepositoryImpl
+) {
 
     //C
+    fun create(todoDto: TodoDto) {
+
+    }
 
     //R
 
