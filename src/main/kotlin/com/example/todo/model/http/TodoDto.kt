f432d7c8b9b5c4cb9236966302fa86715f5a5edb
mck0324 <dch3030@naver.com>
2023-08-14 17:05:42 +0900

    Todo->TodoDTO Convert

--- src/main/kotlin/com/example/todo/model/http/TodoDto.kt
+++ src/main/kotlin/com/example/todo/model/http/TodoDto.kt
@@ -1,5 +1,6 @@
 package com.example.todo.model.http
 
+import com.example.todo.database.Todo
 import jakarta.validation.constraints.AssertTrue
 import jakarta.validation.constraints.NotBlank
 import java.time.LocalDateTime
@@ -30,5 +31,15 @@ data class TodoDto (
                 } catch (e: Exception) {
                         false
                 }
+        }
+}
+fun Todo.convertTodo(todo: Todo): TodoDto {
+        return TodoDto().apply {
+                this.index = todo.index
+                this.title = todo.title
+                this.description = todo.description
+                this.schedule = todo.schedule?.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+                this.createdAt = todo.createdAt
+                this.updatedAt = todo.updatedAt
         }
 }
